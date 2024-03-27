043c4d97252fa774bd98dd657a89fe5b7db0d467
Petbenes <129893069+Petbenes@users.noreply.github.com>
2024-03-27 13:23:54 +0100

    Add files via upload

---
+++ Kalkulacka2024/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Kalkulacka2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btTlacitko7 = new System.Windows.Forms.Button();
+            this.btTlacitko8 = new System.Windows.Forms.Button();
+            this.btTlacitko9 = new System.Windows.Forms.Button();
+            this.btTlacitko4 = new System.Windows.Forms.Button();
+            this.btTlacitko5 = new System.Windows.Forms.Button();
+            this.btTlacitko6 = new System.Windows.Forms.Button();
+            this.btTlacitko3 = new System.Windows.Forms.Button();
+            this.btTlacitko2 = new System.Windows.Forms.Button();
+            this.btTlacitko1 = new System.Windows.Forms.Button();
+            this.btTlacitko0 = new System.Windows.Forms.Button();
+            this.btSmazat = new System.Windows.Forms.Button();
+            this.btTlacitkoPlus = new System.Windows.Forms.Button();
+            this.btTlacitkoMinus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtDisplay.Location = new System.Drawing.Point(12, 12);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(394, 29);
+            this.txtDisplay.TabIndex = 0;
+            this.txtDisplay.Text = "0";
+            this.txtDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btTlacitko7
+            // 
+            this.btTlacitko7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btTlacitko7.Location = new System.Drawing.Point(12, 58);
+            this.btTlacitko7.Name = "btTlacitko7";
+            this.btTlacitko7.Size = new System.Drawing.Size(66, 59);
+            this.btTlacitko7.TabIndex = 1;
+            this.btTlacitko7.Text = "7";
+            this.btTlacitko7.UseVisualStyleBackColor = true;
+            this.btTlacitko7.Click += new System.EventHandler(this.btTlacitko0_Click);
+            // 
+            // btTlacitko8
+            // 
+            this.btTlacitko8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btTlacitko8.Location = new System.Drawing.Point(96, 58);
+            this.btTlacitko8.Name = "btTlacitko8";
+            this.btTlacitko8.Size = new System.Drawing.Size(66, 59);
+            this.btTlacitko8.TabIndex = 2;
+            this.btTlacitko8.Text = "8";
+            this.btTlacitko8.UseVisualStyleBackColor = true;
+            this.btTlacitko8.Click += new System.EventHandler(this.btTlacitko0_Click);
+            // 
+            // btTlacitko9
+            // 
+            this.btTlacitko9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btTlacitko9.Location = new System.Drawing.Point(180, 57);
+            this.btTlacitko9.Name = "btTlacitko9";
+            this.btTlacitko9.Size = new System.Drawing.Size(66, 59);
+            this.btTlacitko9.TabIndex = 3;
+            this.btTlacitko9.Text = "9";
+            this.btTlacitko9.UseVisualStyleBackColor = true;
+            this.btTlacitko9.Click += new System.EventHandler(this.btTlacitko0_Click);
+            // 
+            // btTlacitko4
+            // 
+            this.btTlacitko4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btTlacitko4.Location = new System.Drawing.Point(12, 123);
+            this.btTlacitko4.Name = "btTlacitko4";
+            this.btTlacitko4.Size = new System.Drawing.Size(66, 59);
+            this.btTlacitko4.TabIndex = 4;
+            this.btTlacitko4.Text = "4";
+            this.btTlacitko4.UseVisualStyleBackColor = true;
+            this.btTlacitko4.Click += new System.EventHandler(this.btTlacitko0_Click);
+            // 
+            // btTlacitko5
+            // 
+            this.btTlacitko5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btTlacitko5.Location = new System.Drawing.Point(96, 123);
+            this.btTlacitko5.Name = "btTlacitko5";
+            this.btTlacitko5.Size = new System.Drawing.Size(66, 59);
+            this.btTlacitko5.TabIndex = 5;
+            this.btTlacitko5.Text = "5";
+            this.btTlacitko5.UseVisualStyleBackColor = true;
+            this.btTlacitko5.Click += new System.EventHandler(this.btTlacitko0_Click);
+            // 
+            // btTlacitko6
+            // 
+            this.btTlacitko6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btTlacitko6.Location = new System.Drawing.Point(180, 122);
+            this.btTlacitko6.Name = "btTlacitko6";
+            this.btTlacitko6.Size = new System.Drawing.Size(66, 59);
+            this.btTlacitko6.TabIndex = 6;
+            this.btTlacitko6.Text = "6";
+            this.btTlacitko6.UseVisualStyleBackColor = true;
+            this.btTlacitko6.Click += new System.EventHandler(this.btTlacitko0_Click);
+            // 
+            // btTlacitko3
+            // 
+            this.btTlacitko3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btTlacitko3.Location = new System.Drawing.Point(180, 187);
+            this.btTlacitko3.Name = "btTlacitko3";
+            this.btTlacitko3.Size = new System.Drawing.Size(66, 59);
+            this.btTlacitko3.TabIndex = 9;
+            this.btTlacitko3.Text = "3";
+            this.btTlacitko3.UseVisualStyleBackColor = true;
+            this.btTlacitko3.Click += new System.EventHandler(this.btTlacitko0_Click);
+            // 
+            // btTlacitko2
+            // 
+            this.btTlacitko2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btTlacitko2.Location = new System.Drawing.Point(96, 188);
+            this.btTlacitko2.Name = "btTlacitko2";
+            this.btTlacitko2.Size = new System.Drawing.Size(66, 59);
+            this.btTlacitko2.TabIndex = 8;
+            this.btTlacitko2.Text = "2";
+            this.btTlacitko2.UseVisualStyleBackColor = true;
+            this.btTlacitko2.Click += new System.EventHandler(this.btTlacitko0_Click);
+            // 
+            // btTlacitko1
+            // 
+            this.btTlacitko1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btTlacitko1.Location = new System.Drawing.Point(12, 188);
+            this.btTlacitko1.Name = "btTlacitko1";
+            this.btTlacitko1.Size = new System.Drawing.Size(66, 59);
+            this.btTlacitko1.TabIndex = 7;
+            this.btTlacitko1.Text = "1";
+            this.btTlacitko1.UseVisualStyleBackColor = true;
+            this.btTlacitko1.Click += new System.EventHandler(this.btTlacitko0_Click);
+            // 
+            // btTlacitko0
+            // 
+            this.btTlacitko0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btTlacitko0.Location = new System.Drawing.Point(12, 253);
+            this.btTlacitko0.Name = "btTlacitko0";
+            this.btTlacitko0.Size = new System.Drawing.Size(150, 59);
+            this.btTlacitko0.TabIndex = 10;
+            this.btTlacitko0.Text = "0";
+            this.btTlacitko0.UseVisualStyleBackColor = true;
+            this.btTlacitko0.Click += new System.EventHandler(this.btTlacitko0_Click);
+            // 
+            // btSmazat
+            // 
+            this.btSmazat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btSmazat.Location = new System.Drawing.Point(180, 252);
+            this.btSmazat.Name = "btSmazat";
+            this.btSmazat.Size = new System.Drawing.Size(66, 59);
+            this.btSmazat.TabIndex = 11;
+            this.btSmazat.Text = "C";
+            this.btSmazat.UseVisualStyleBackColor = true;
+            this.btSmazat.Click += new System.EventHandler(this.btSmazat_Click);
+            // 
+            // btTlacitkoPlus
+            // 
+            this.btTlacitkoPlus.Location = new System.Drawing.Point(333, 58);
+            this.btTlacitkoPlus.Name = "btTlacitkoPlus";
+            this.btTlacitkoPlus.Size = new System.Drawing.Size(73, 59);
+            this.btTlacitkoPlus.TabIndex = 12;
+            this.btTlacitkoPlus.Text = "+";
+            this.btTlacitkoPlus.UseVisualStyleBackColor = true;
+            // 
+            // btTlacitkoMinus
+            // 
+            this.btTlacitkoMinus.Location = new System.Drawing.Point(333, 123);
+            this.btTlacitkoMinus.Name = "btTlacitkoMinus";
+            this.btTlacitkoMinus.Size = new System.Drawing.Size(73, 59);
+            this.btTlacitkoMinus.TabIndex = 13;
+            this.btTlacitkoMinus.Text = "-";
+            this.btTlacitkoMinus.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 477);
+            this.Controls.Add(this.btTlacitkoMinus);
+            this.Controls.Add(this.btTlacitkoPlus);
+            this.Controls.Add(this.btSmazat);
+            this.Controls.Add(this.btTlacitko0);
+            this.Controls.Add(this.btTlacitko3);
+            this.Controls.Add(this.btTlacitko2);
+            this.Controls.Add(this.btTlacitko1);
+            this.Controls.Add(this.btTlacitko6);
+            this.Controls.Add(this.btTlacitko5);
+            this.Controls.Add(this.btTlacitko4);
+            this.Controls.Add(this.btTlacitko9);
+            this.Controls.Add(this.btTlacitko8);
+            this.Controls.Add(this.btTlacitko7);
+            this.Controls.Add(this.txtDisplay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kalkulačka";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btTlacitko7;
+        private System.Windows.Forms.Button btTlacitko8;
+        private System.Windows.Forms.Button btTlacitko9;
+        private System.Windows.Forms.Button btTlacitko4;
+        private System.Windows.Forms.Button btTlacitko5;
+        private System.Windows.Forms.Button btTlacitko6;
+        private System.Windows.Forms.Button btTlacitko3;
+        private System.Windows.Forms.Button btTlacitko2;
+        private System.Windows.Forms.Button btTlacitko1;
+        private System.Windows.Forms.Button btTlacitko0;
+        private System.Windows.Forms.Button btSmazat;
+        private System.Windows.Forms.Button btTlacitkoPlus;
+        private System.Windows.Forms.Button btTlacitkoMinus;
+    }
+}
+
---
+++ Kalkulacka2024/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulacka2024
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // -----------------------------------------
+        // Vypisuje stisknutá tlačítka na display
+        // -----------------------------------------
+        private void btTlacitko0_Click(object sender, EventArgs e)
+        {
+            Button lbtMojeTlacitko;
+
+            lbtMojeTlacitko = (Button)sender;
+
+            // test zda tam není samotná nula
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = "";
+            }
+
+            // vypsat číslo na display
+            txtDisplay.Text = txtDisplay.Text + lbtMojeTlacitko.Text;
+        }
+
+        // -----------------------------------------
+        // Vymazat vše na kalkulačce
+        // -----------------------------------------
+        private void btSmazat_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "0";
+        }
+    }
+}
